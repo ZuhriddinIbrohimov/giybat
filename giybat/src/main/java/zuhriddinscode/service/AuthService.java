f4692f95d4cb78b5726f8685af79e585107cd073
ZuhriddinIbrohimov <151710292+ZuhriddinIbrohimov@users.noreply.github.com>
2025-04-10 16:09:53 +0500

    0.4

--- giybat/src/main/java/zuhriddinscode/service/AuthService.java
+++ giybat/src/main/java/zuhriddinscode/service/AuthService.java
@@ -1,11 +1,15 @@
 package zuhriddinscode.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import zuhriddinscode.dto.RegistrationDTO;
 import zuhriddinscode.entity.ProfileEntity;
 import zuhriddinscode.exps.AppBadException;
 import zuhriddinscode.repository.ProfileRepository;
+import zuhriddinscode.types.GeneralStatus;
+
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -14,16 +18,29 @@ public class AuthService {
     @Autowired
     private ProfileRepository profileRepository;
 
-    public String registration ( RegistrationDTO registrationDTO ){
-    // 1. validation
-    // 2. user mb da bormi ?
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-       Optional<ProfileEntity>  optional = profileRepository.findByUsernameAndVisibleTrue(registrationDTO.getUsername());
-       if (optional.isEmpty()) {
-           throw new AppBadException("Username already exists");
-       }
+    public String registration(RegistrationDTO registrationDTO) {
+        // 1. validation
+        // 2. user mb da bormi ?
 
-
-        return "successfully registered";
+        Optional<ProfileEntity> optional = profileRepository.findByUsernameAndVisibleTrue(registrationDTO.getUsername());
+        if (optional.isEmpty()) {
+            ProfileEntity profile = optional.get();
+            if (profile.getStatus().equals(GeneralStatus.IN_REGISTRATION)) {
+                profileRepository.delete(profile);
+            } else {
+                throw new AppBadException("Username already exists");
+            }
+        }
+        ProfileEntity entity = new ProfileEntity();
+        entity.setName(registrationDTO.getName());
+        entity.setUsername(registrationDTO.getUsername());
+        entity.setPassword(bCryptPasswordEncoder.encode(registrationDTO.getPassword()));
+        entity.setStatus(GeneralStatus.IN_REGISTRATION);
+        entity.setCreatedDate(LocalDateTime.now());
+        profileRepository.save(entity);
+        return "Successfully registered";
     }
 }
