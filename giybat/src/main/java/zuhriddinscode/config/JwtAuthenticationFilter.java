6bed32f7dce6d98d3b0470b9ddc67d5ff13ec19d
ZuhriddinIbrohimov <ibrohimovzuhriddin310@gmail.com>
2025-05-18 09:59:55 +0500

    1.0.7

--- giybat/src/main/java/zuhriddinscode/config/JwtAuthenticationFilter.java
+++ giybat/src/main/java/zuhriddinscode/config/JwtAuthenticationFilter.java
@@ -17,7 +17,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import zuhriddinscode.dto.JwtDTO;
 import zuhriddinscode.util.JwtUtil;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 @Component
